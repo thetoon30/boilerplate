ab29df0c09d97fafae173c3d3a19a3c34ef8f841
toon <toon.b@excelbangkok.com>
2018-12-24 10:09:24 +0700

    Provider TokenManager TokenValidataion

---
+++ MVC5/src/InternalModule/Providers/TokenManager.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+namespace InternalModule.Boilerplate.Providers
+{
+    public class TokenManager
+    {
+        private static string jwtSecret = ConfigurationManager.AppSettings["jwtSecret"];
+
+        public static string GenerateToken(string username)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(jwtSecret);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+
+
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, username)}),
+
+                Expires = DateTime.Now.AddMinutes(30),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+            //token.Payload["favouriteFood"] = "cheese";
+            return handler.WriteToken(token);
+        }
+
+        public static string ValidateToken(string token)
+        {
+            string username = null;
+            ClaimsPrincipal principal = GetPrincipal(token);
+
+            if (principal == null)
+                return null;
+
+            ClaimsIdentity identity = null;
+            try
+            {
+                identity = (ClaimsIdentity)principal.Identity;
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+
+            //Claim usernameClaim = identity.FindFirst("userid");
+            Claim usernameClaim = identity.FindFirst(ClaimTypes.Name);
+            username = usernameClaim.Value;
+
+            return username;
+        }
+
+        public static ClaimsPrincipal GetPrincipal(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+                var aa = jwtToken.Claims;
+                if (jwtToken == null)
+                    return null;
+
+                byte[] key = Encoding.UTF8.GetBytes(jwtSecret);
+
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    ValidateLifetime = false,
+                    RequireExpirationTime = false,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+                };
+
+                SecurityToken securityToken;
+
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
+
+                return principal;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ MVC5/src/InternalModule/Providers/TokenValidationHandler.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace InternalModule.Boilerplate.Providers
+{
+    internal class TokenValidationHandler : DelegatingHandler
+    {
+        private static string jwtSecret = ConfigurationManager.AppSettings["jwtSecret"];
+
+        private static bool TryRetrieveToken(HttpRequestMessage request, out string token)
+        {
+            token = null;
+            IEnumerable<string> authzHeaders;
+            if (!request.Headers.TryGetValues("Authorization", out authzHeaders) || authzHeaders.Count() > 1)
+            {
+                return false;
+            }
+            var bearerToken = authzHeaders.ElementAt(0);
+            token = bearerToken.StartsWith("Bearer ") ? bearerToken.Substring(7) : bearerToken;
+            return true;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpStatusCode statusCode;
+            string token;
+            //determine whether a jwt exists or not
+            if (!TryRetrieveToken(request, out token))
+            {
+                statusCode = HttpStatusCode.Unauthorized;
+                //allow requests with no token - whether a action method needs an authentication can be set with the claimsauthorization attribute
+                return base.SendAsync(request, cancellationToken);
+            }
+
+            try
+            {
+                var now = DateTime.UtcNow;
+
+                SecurityToken securityToken;
+                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+                TokenValidationParameters validationParameters = new TokenValidationParameters()
+                {
+                    ValidateLifetime = false,
+                    RequireExpirationTime = false,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtSecret))
+                };
+                //extract and assign the user of the jwt
+                Thread.CurrentPrincipal = handler.ValidateToken(token, validationParameters, out securityToken);
+                HttpContext.Current.User = handler.ValidateToken(token, validationParameters, out securityToken);
+
+                return base.SendAsync(request, cancellationToken);
+            }
+            catch (SecurityTokenValidationException e)
+            {
+                statusCode = HttpStatusCode.Unauthorized;
+            }
+            catch (Exception ex)
+            {
+                statusCode = HttpStatusCode.Unauthorized;
+            }
+
+            return Task<HttpResponseMessage>.Factory.StartNew(() => new HttpResponseMessage(statusCode) { });
+        }
+
+        public bool LifetimeValidator(DateTime? notBefore, DateTime? expires, SecurityToken securityToken, TokenValidationParameters validationParameters)
+        {
+            if (expires != null)
+            {
+                if (DateTime.UtcNow < expires) return true;
+            }
+            return false;
+        }
+    }
+}
