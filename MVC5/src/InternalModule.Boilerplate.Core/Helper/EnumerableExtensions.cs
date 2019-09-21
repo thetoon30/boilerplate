f1dd59fae7ae46542046e090de3105c7ef8c78c7
thetoon30 <thetoon30@gmail.com>
2019-09-21 15:24:03 +0700

    Swap item in list

--- MVC5/src/InternalModule.Boilerplate.Core/Helper/EnumerableExtensions.cs
+++ MVC5/src/InternalModule.Boilerplate.Core/Helper/EnumerableExtensions.cs
@@ -24,5 +24,13 @@ namespace InternalModule.Boilerplate.Core.Helper
                 yield return element;
             }
         }
+
+        public static IList<T> Swap<T>(this IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+            return list;
+        }
     }
 }
