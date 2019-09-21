8be63df807754164c0fd80b2209d9626da4e4d42
thetoon30 <thetoon30@gmail.com>
2019-09-21 15:37:30 +0700

    QueryableExtension

---
+++ MVC5/src/InternalModule.Boilerplate.Core/Helper/QueryableExtension/QueryableCondition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalModule.Boilerplate.Core.Helper
+{
+    public class QueryableCondition<TSource>
+    {
+        private readonly IQueryable<TSource> queryable;
+        private readonly bool conditionState;
+
+        public QueryableCondition(IQueryable<TSource> queryable, bool conditionState)
+        {
+            this.queryable = queryable;
+            this.conditionState = conditionState;
+        }
+
+        public IQueryable<TSource> Where(Expression<Func<TSource, bool>> predicate)
+        {
+            return conditionState ? queryable.Where(predicate) : queryable;
+        }
+    }
+}
---
+++ MVC5/src/InternalModule.Boilerplate.Core/Helper/QueryableExtension/QueryableExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalModule.Boilerplate.Core.Helper
+{
+    public static class QueryableExtension
+    {
+        public static QueryableCondition<TSource> If<TSource>(this IQueryable<TSource> queryable, bool condition)
+        {
+            return new QueryableCondition<TSource>(queryable, condition);
+        }
+        public static IQueryable<TSource> OrderBy<TSource>(this IQueryable<TSource> source, string column, bool type)
+        {
+            if (string.IsNullOrEmpty(column)) { return source; }
+            var expression = source.Expression;
+            var parameter = Expression.Parameter(typeof(TSource), "x");
+            var selector = Expression.PropertyOrField(parameter, column);
+            var method = type ? "OrderBy" : "OrderByDescending";
+            expression = Expression.Call(typeof(Queryable), method,
+                    new Type[] { source.ElementType, selector.Type },
+                    expression, Expression.Quote(Expression.Lambda(selector, parameter)));
+            return source.Provider.CreateQuery<TSource>(expression);
+        }
+    }
+}
